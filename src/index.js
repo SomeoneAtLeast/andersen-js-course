a9511eff012437604bc30eadca300dd0f5a67308
Вадим Бабичев <WineCooler@yandex.ru>
2021-07-29 20:39:41 +0300

    Очищен index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './styles/main.css';
-
-const getResolvedPromise = value => {
-  return Promise.resolve(value)
-    .then(result => {
-      if (result > 300) throw new Error('Ошибка');
-    })
-    .catch(e => console.log(e))
-    .finally(() => console.log('This is Finally!'));
-};
-
-getResolvedPromise(500);
