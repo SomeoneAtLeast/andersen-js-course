df067e1b75f73a9a8713e9c17bbc16bb635bd6aa
Вадим Бабичев <WineCooler@yandex.ru>
2021-07-29 17:58:24 +0300

    Выполнено: asynchronous-programming task2

---
+++ src/asynchronous-programming/task2.js
@@ -0,0 +1,21 @@
+const parseJSON = (jsonStr, successCb, failureCb) => {
+    try {
+        const result = JSON.parse(jsonStr);
+        successCb(result);
+    } catch (e) {
+        failureCb(e);
+    }
+};
+
+const successCb = result => {
+    console.log("Success parse!");
+    console.log(result);
+};
+
+const failureCb = error => {
+    console.log("Failure parse!");
+    console.log(error);
+};
+
+parseJSON('{"x": 10}', successCb, failureCb);
+parseJSON('{x}', successCb, failureCb);
