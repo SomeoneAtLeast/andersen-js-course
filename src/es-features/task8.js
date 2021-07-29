3018d5e0fc844bff3b5d74ec14c7d6eddae610ad
Вадим Бабичев <WineCooler@yandex.ru>
2021-07-29 14:52:11 +0300

    Выполнено: es-features task8

--- src/es-features/task8.js
+++ src/es-features/task8.js
@@ -28,4 +28,13 @@ export function task8Old() {
 // Напишите реализацию функции task8Old на ES6+ ниже этого комментария.
 // При желании, можете использовать стрелочную функцию, вместо обычной
 
-export function task8New() {}
+export function task8New() {
+  const obj = {
+    nums: [0, 2, 5, 10, 15],
+    calculateNumsFive() {
+      return this.nums.filter(n => n % 5 === 0);
+    },
+  };
+
+  return obj.calculateNumsFive();
+}
